e29f922037e4bad0a289bef0a4886ddd3130c2b6
HollyLgru <H.grudovich@zollege.com>
2022-08-23 18:21:20 -0500

    MATERIAL UI WORKING

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Container, Table, TableBody, TableCell, TableHead, TableRow, Button, Menu, MenuItem } from '@material-ui/core';
+import { MoreVert } from '@material-ui/icons';
 
 const Import = (props) => {
     // fill out this component
 
     return (
-        <p>Import Component</p>
+        <container>
+            <p>Import Component</p>
+            <Button variant="contained" color="primary">
+				Import
+			</Button>
+        </container>
     )
 }
 
