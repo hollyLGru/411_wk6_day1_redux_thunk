f7f8e1e5087d2b54e7830b0ef29c323ec2ec3742
HollyLgru <H.grudovich@zollege.com>
2022-08-23 20:12:14 -0500

    im done and I hated this assignment

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -16,12 +16,16 @@ const cars = (state = [], action) => {
 }
 
 const makes = (state = [], action) => {
-    switch(action.type) {
-        case 'FETCH_MAKES' :
-            return action.value 
-        default:
-            return state
-    }
-}
+	switch (action.type) {
+		case 'FETCH_MAKES':
+			return action.value;
+		case 'DELETE_MAKE':
+			const makes = [...state];
+			makes.splice(action.value, 1);
+			return makes;
+		default:
+			return state;
+	}
+};
 
 export default combineReducers({ user, cars, makes })
